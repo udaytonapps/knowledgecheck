a2d039a8e245f4f335aa3e97ff5464da81ddfeb6
David P. Bauer <dbauer1@udayton.edu>
2017-10-16 10:34:15 -0400

    Initial commit from template

---
+++ database.php
@@ -0,0 +1,28 @@
+<?php
+
+// The SQL to uninstall this tool
+$DATABASE_UNINSTALL = array(
+    "drop table if exists {$CFG->dbprefix}/*Tool Table*/"
+);
+
+// The SQL to create the tables if they don't exist
+$DATABASE_INSTALL = array(
+    array( "{$CFG->dbprefix}/*Tool Table*/",
+        "create table {$CFG->dbprefix}/*Tool Table*/ (
+    link_id     INTEGER NOT NULL,
+    user_id     INTEGER NOT NULL,
+    /* Columns for the tool go here */
+
+    CONSTRAINT `{$CFG->dbprefix}/*Tool Table*/_ibfk_1`
+        FOREIGN KEY (`link_id`)
+        REFERENCES `{$CFG->dbprefix}lti_link` (`link_id`)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT `{$CFG->dbprefix}/*Tool Table*/_ibfk_2`
+        FOREIGN KEY (`user_id`)
+        REFERENCES `{$CFG->dbprefix}lti_user` (`user_id`)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+
+    UNIQUE(link_id, user_id)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8")
+);
---
+++ register.php
@@ -0,0 +1,9 @@
+<?php
+
+$REGISTER_LTI2 = array(
+    "name" => "", // Name of the tool
+    "FontAwesome" => "", // Icon for the tool
+    "short_name" => "",
+    "description" => "", // Tool description
+    "messages" => array("launch", "launch_grade")
+);
